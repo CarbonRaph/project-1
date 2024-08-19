aa9e40bbf105a0b90e4b8e12b96cef0f4efb1773
CarbonRaph <117633952+CarbonRaph@users.noreply.github.com>
2024-08-19 15:21:46 +0000

    Update manage.py again

--- manage.py
+++ manage.py
@@ -1,23 +1,30 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+# #!/usr/bin/env python
+# """Django's command-line utility for administrative tasks."""
+# import os
+# import sys
+
+
+# def main():
+#     """Run administrative tasks."""
+#     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+#     try:
+#         from django.core.management import execute_from_command_line
+#     except ImportError as exc:
+#         raise ImportError(
+#             "Couldn't import Django. Are you sure it's installed and "
+#             "available on your PYTHONPATH environment variable? Did you "
+#             "forget to activate a virtual environment?"
+#         ) from exc
+#     port = os.getenv('DJANGO_PORT', '8000')  # Default to 8000 if not set
+#     execute_from_command_line(['manage.py', 'runserver', f'0.0.0.0:{port}'])
+
+
+# if __name__ == '__main__':
+#     main()
+
+from django.core.management import execute_from_command_line
 import os
-import sys
 
-
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+if __name__ == "__main__":
     port = os.getenv('DJANGO_PORT', '8000')  # Default to 8000 if not set
     execute_from_command_line(['manage.py', 'runserver', f'0.0.0.0:{port}'])
-
-
-if __name__ == '__main__':
-    main()
